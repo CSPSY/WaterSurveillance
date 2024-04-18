e573001e1c6c43385385c7e1cff619921bb3df02
CSPSY <1554830585@qq.com>
2024-04-18 18:56:58 +0800

    feat: api 接口鉴权

--- src/api/base.js
+++ src/api/base.js
@@ -7,4 +7,13 @@ const API = axios.create({
     baseURL: '/api'
 });
 
+// API 请求拦截器
+API.interceptors.request.use((req) => {
+    const wsvToken = localStorage.getItem('WSV_TOKEN');
+    if (wsvToken) {
+        req.headers.Authorization = wsvToken;
+    }
+    return req;
+});
+
 export default API;
